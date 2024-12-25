6afaf323d3e4608650b9920130fab07e75458993
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-24 21:57:40 -1000

    Update Narcissistic-number.js

--- Narcissistic-number.js
+++ Narcissistic-number.js
@@ -19,3 +19,9 @@ only valid positive non-zero integers will be passed into the function.
 
 
 // Solution
+
+function narcissistic( value ) {
+  return ('' + value).split('').reduce(function(p, c){
+    return p + Math.pow(c, ('' + value).length)
+    }, 0) == value;
+}
