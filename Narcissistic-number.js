0d6fb7ebb80956f2337afd9122b1c0b51a0cbee3
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-24 21:58:22 -1000

    Update Narcissistic-number.js

--- Narcissistic-number.js
+++ Narcissistic-number.js
@@ -27,3 +27,11 @@ function narcissistic( value ) {
 }
 
 // or
+
+function narcissistic(value) {
+  return value.toString()
+              .split('')
+              .map( (x,i,arr) => x ** arr.length)
+              .reduce( (a,b)=> +a + +b) 
+               === value
+}
